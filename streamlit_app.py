6d6b58aeed3127a4652af4ef81ce6c65bafa8363
ssn9051999 <123964361+ssn9051999@users.noreply.github.com>
2023-01-30 20:55:47 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
 import streamlit
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
-streamlit.title('1.Idli\n2.Dosa\n3.Sambhar')
+streamlit.text('1.Idli\n2.Dosa\n3.Sambhar')
