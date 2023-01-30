612a71b37e0e25ff48c21f47dae8074b580c25dd
ssn9051999 <123964361+ssn9051999@users.noreply.github.com>
2023-01-30 20:58:32 +0530

    Update streamlit_app.py
    added breakfast and lunch menu header and 3 items each

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,7 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
 streamlit.text('1.Idli\n2.Dosa\n3.Sambhar')
+streamlit.header('Lunch Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
