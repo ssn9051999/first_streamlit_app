bc30fd0da7fc2aed66183ac73ad10a013eaacb8f
ssn9051999 <123964361+ssn9051999@users.noreply.github.com>
2023-02-01 12:51:52 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,6 +33,6 @@ fruit_choice = streamlit.text_input('What fruit would you like information about
 streamlit.write('The user entered ', fruit_choice)
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"kiwi") 
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice) 
 fruityvice_normalized = pd.json_normalize(fruityvice_response.json()) 
 streamlit.dataframe(fruityvice_normalized)
