5a5c23d39c0b6c8d7d0d417a463471282b7e90c8
ssn9051999 <123964361+ssn9051999@users.noreply.github.com>
2023-02-01 18:43:05 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,7 +42,7 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_ch
 fruityvice_normalized = pd.json_normalize(fruityvice_response.json()) 
 streamlit.dataframe(fruityvice_normalized)
 
-#don't run anything past here while we troubleshoot
+#don't run anything past here while we troubleshoot okay?
 streamlit.stop()
 
 
