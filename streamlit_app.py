83ead844ff8773d7cef5589d96228ec5510923c9
ssn9051999 <123964361+ssn9051999@users.noreply.github.com>
2023-02-01 12:29:40 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
-#New section to display Fruityvice Fruit Advice!
+#New section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
 
 import requests
